dd8102a41b18d6b8c571cf283812e43c452119fc
kanniyappan01 <kanniyappan646@gmail.com>
2023-01-13 12:39:15 +0530

    adding new products

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-
 let data=[
 	{
 		id: 1,
 		img:"images/shoe-01.webp",
 		productName: "Air forces",
 		price: 800,
+		category: "shoe"
 	},
 	{
 		id: 2,
 		img:"images/shoe-02.webp",
 		productName: "Bast shoe",
 		price: 1200,
+		category: "shoe"
 		
 	},
 	{
@@ -18,43 +19,161 @@ let data=[
 		img:"images/shoe-03.webp",
 		productName: "Blucher shoe",
 		price: 1500,
+		category: "shoe"
 	},
 	{
 		id: 4,
 		img:"images/shoe-04.webp",
 		productName: "Boat shoe",
 		price: 600,
+		category: "shoe"
 	},
 	{
 		id: 5,
 		img:"images/shoe-05.webp",
 		productName: "Brogue shoe",
 		price: 400,
+		category: "shoe"
 	},
 	{
 		id: 6,
 		img:"images/shoe-06.webp",
 		productName: "Diabetic shoe",
 		price: 550,
+		category: "shoe"
 	},
 	{
 		id: 7,
 		img:"images/shoe-07.webp",
 		productName: "Galesh",
 		price: 950,
+		category: "shoe"
 	},
 	{
 		id: 8,
 		img:"images/shoe-08.webp",
 		productName: "Giveh",
 		price: 450,
+		category: "shoe"
+	},
+	{
+		id: 9,
+		img:"images/t-shirt-01.webp",
+		productName: "White T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 10,
+		img:"images/t-shirt-02.webp",
+		productName: "Yellow T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 11,
+		img:"images/t-shirt-03.webp",
+		productName: "Dark Green T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 12,
+		img:"images/t-shirt-04.webp",
+		productName: "Green T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 13,
+		img:"images/t-shirt-05.webp",
+		productName: "Fether T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 14,
+		img:"images/t-shirt-08.webp",
+		productName: "Rock T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 15,
+		img:"images/t-shirt-09.webp",
+		productName: "OXO T-Shirt",
+		price: 450,
+		category: "tshirt"
+	},
+	{
+		id: 16,
+		img:"images/flip-flop-01.webp",
+		productName: "Adda",
+		price: 450,
+		category: "flipflop"
+	},
+	{
+		id: 17,
+		img:"images/flip-flop-02.webp",
+		productName: "black Line Adda",
+		price: 450,
+		category: "flipflop"
+	},
+	{
+		id: 18,
+		img:"images/flip-flop-03.webp",
+		productName: "Adda Deep",
+		price: 450,
+		category: "flipflop"
+	},
+	{
+		id: 19,
+		img:"images/flip-flop-04.webp",
+		productName: "black Strip adam",
+		price: 450,
+		category: "flipflop"
+	},
+	{
+		id: 20,
+		img:"images/boat-01.webp",
+		productName: "black Boat",
+		price: 450,
+		category: "headset"
+	},
+	{
+		id: 21,
+		img:"images/boat-02.webp",
+		productName: "Hash Boat",
+		price: 450,
+		category: "headset"
+	},
+	{
+		id: 22,
+		img:"images/boat-03.webp",
+		productName: "Brown Boat",
+		price: 450,
+		category: "headset"
+	},
+	{
+		id: 23,
+		img:"images/boat-04.webp",
+		productName: "black Boat",
+		price: 450,
+		category: "headset"
+	},
+	{
+		id: 24,
+		img:"images/boat-05.webp",
+		productName: "Hash Boat",
+		price: 450,
+		category: "headset"
 	}
 ];
 var totals=0;
 var foodContainer = document.getElementById("product-container");
 
 for(i=0;i<data.length;i++){
-	foodContainer.innerHTML += `<div class="card m-2 position-relative">
+	foodContainer.innerHTML += `<div class="card m-2 position-relative" data-item="${data[i].category}">
 									<img class="cart-img" src="${data[i].img}" alt="shoe images">
 									<div class="card-body">
 										<h4 class="card-title">${data[i].productName}</h4>
@@ -85,7 +204,7 @@ addCart.forEach((btn)=>{
 
 var cartCount = document.querySelector(".cart-number");
 let itemList =[],newProduct;
-
+ 
 function addCard(){
 	let food = this.parentNode;
 	let title = food.querySelector(".card-title").innerText;
@@ -102,14 +221,10 @@ function addCard(){
 		return;
 	}else{
 		itemList.push(newProduct);
-		
 	}
 	cartCount.textContent = itemList.length;
 	
-	
 	createCartProudct(title, price, imgSrc);
-	
-	
 	 
 }
 var cartBasket = document.querySelector(".card-items-container");
@@ -144,36 +259,108 @@ function createCartProudct(title, price, imgSrc){
 	for(let i = 0; i < decrementBtn.length; i++){ 
 		decrementBtn[i].addEventListener("click", quantity)
 	}
-	totals+=price
-	console.log(price)
+	// totals+=price
 	
 	var btnremove = document.querySelectorAll(".delete-icon");
 		btnremove.forEach((btn)=>{
 			btn.addEventListener("click", removeItem);
 		});
+	updateTotal()
 	
 }
-
 //delete cartlist
-function removeItem(){
+function removeItem(i){
 	if(confirm("are you sure to remove")){
-		console.log(itemList);
 		let title = this.parentElement.parentElement.querySelector(".product-title").innerText;
+		
 		itemList=itemList.filter((el)=>{
 			el.title != title
 		});
-		this.parentElement.remove();	
+		this.parentElement.remove();
 	}
 }
+let buy = document.querySelector(".buy-now");
+let done = document.querySelector(".done");
 
-function quantity(qn){
+// buy.addEventListener("click",function(){
+// 	setTimeout(popUp, 100)
+// 	done.classList.add("active-done");
+// 	done.style.display = "block";
+// });
+//product filter start
 
-	let _parentNode, contr, prdTlPrice, prdInPrice,prTotal,totalAmt,totalPrice=0,cnt=0;
-	if(qn.target.dataset.target == "incr"){
-	for (let i =0; i<itemList.length;i++){
+let searchBox = document.querySelector("#search");
+let selectButton = document.querySelectorAll(".product-btn");
+let cardBoxes = document.querySelectorAll(".card");
+
+//search product by textbox
+selectButton[0].classList.add("select-active");
+searchBox.addEventListener("keyup",(e)=>{
+	let searchText = e.target.value.toLowerCase().trim();
+	
+	for(i=0;i<cardBoxes.length;i++){
+		let data = cardBoxes[i].dataset.item;
+		if(data.includes(searchText)){
+			cardBoxes[i].style.display = "block";
+		}else{
+			cardBoxes[i].style.display = "none";
+		}
+	};
+	for(i=0;i<selectButton.length;i++){
+		selectButton[i].classList.remove("select-active");
+	}
+	// e.target.classList.add("select-active");
+	
+	selectButton[i].classList.add("select-active");
+});
+for(i=0;i<selectButton.length;i++){
+	selectButton[i].addEventListener("click",(e)=>{
+		e.preventDefault();
+		setActiveBtn(e);
+		let selectFilter = e.target.dataset.filter;
+		for(i=0;i<cardBoxes.length;i++){
+			if(selectFilter == "all"){
+				cardBoxes[i].style.display = "block"
+			}else{
+				let boxFilter = cardBoxes[i].dataset.item;
+				
+				if(selectFilter == boxFilter){
+					cardBoxes[i].style.display = "block";
+				}else{
+					cardBoxes[i].style.display = "none"
+				}
+			}
+		}
+	})
+}
+function setActiveBtn(e){
+	for(i=0;i<selectButton.length;i++){
+		selectButton[i].classList.remove("select-active");
+	}
+	e.target.classList.add("select-active");
+}
+//uPdate total
+function updateTotal(){
+	let count=0;
+	let total=0;
+	let carTItems= document.querySelectorAll(".card-box");
+		let totleValue=document.querySelector(".total-price");
+		count++;
 		
-		_parentNode = qn.target.parentNode.parentNode.parentNode;
-		contr = _parentNode.getElementsByClassName("counter")[0];
+		carTItems.forEach(product => {
+
+			let priceEle=product.querySelector(".card-price");
+			let price=parseFloat(priceEle.innerText.replace("₹",""));
+			total += (count*price)
+			totleValue.innerText=total
+		})
+}
+cnt=0;
+function quantity(qn){
+	var _parentNode, contr, prdTlPrice, prdInPrice;
+	if(qn.target.dataset.target == "incr"){
+		let _parentNode = qn.target.parentNode.parentNode.parentNode;
+		let contr = _parentNode.getElementsByClassName("counter")[0];
 		cnt = parseInt(contr.innerText);
 		cnt++;
 		contr.innerText = cnt;
@@ -181,9 +368,7 @@ function quantity(qn){
 		prdInPrice = _parentNode.getElementsByClassName("card-price")[0];
 		prCnt = cnt*prdInPrice.innerText.split("₹")[1];
 		prdTlPrice.innerText = "₹"+prCnt;
-	}
-	}
-	else if(qn.target.dataset.target == "decr"){
+	}else if(qn.target.dataset.target == "decr"){
 		if(cnt > 1){
 			_parentNode = qn.target.parentNode.parentNode.parentNode;
 			contr = _parentNode.getElementsByClassName("counter")[0];
@@ -194,19 +379,7 @@ function quantity(qn){
 			prdInPrice = _parentNode.getElementsByClassName("card-price")[0];
 			prCnt = prdTlPrice.innerText.split("₹")[1]-prdInPrice.innerText.split("₹")[1];
 			prdTlPrice.innerText = "₹"+prCnt;
-		}else{
-			alert("Quantity should be max of 1");
-		}	
+		}
 	}
-}
-let buy = document.querySelector(".buy-now");
-let done = document.querySelector(".done");
-
-buy.addEventListener("click",function(){
-	setTimeout(popUp, 100)
-	done.classList.add("active-done");
-	done.style.display = "block";
-});
-function popUp(){
-	
+	updateTotal()
 }
